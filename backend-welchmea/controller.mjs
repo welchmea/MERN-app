1cbd2b744e16f641beabc8caca932dcf2b27f6dd
Meagan <welchmea@oregonstate.edu>
2023-09-05 12:47:11 -0500

    commit backend files

---
+++ backend-welchmea/controller.mjs
@@ -0,0 +1,91 @@
+import 'dotenv/config';
+import express from 'express';
+import * as vitamins from './model.mjs';
+
+const PORT = process.env.PORT;
+const app = express();
+app.use(express.json()); 
+
+app.post ('/VitaminPage', (req,res) => { 
+    vitamins.createVitamin(
+        req.body.name, 
+        req.body.date, 
+        req.body.purpose,
+        req.body.unit_day
+        )
+        .then(vitamin => {
+            res.status(201).json(vitamin);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(400).json({ error: 'The document was not able to be compiled, check parameters again.' });
+        });
+});
+
+app.get('/VitaminPage', (req, res) => {
+    vitamins.retrieveVitamins()
+        .then(vitamin => { 
+            if (vitamin !== null) {
+                res.json(vitamin);
+            } else {
+                res.status(404).json({ Error: 'The resource you are trying to locate does not exist.' });
+            }         
+         })
+        .catch(error => {
+            console.log(error);
+            res.status(400).json({ Error: 'The document was not able to be compiled, check parameters again.' });
+        });
+});
+
+app.get('/VitaminPage/:_id', (req, res) => {
+    vitamins.retrieveVitaminByID(req.params._id)
+    .then(vitamin => { 
+        if (vitamin !== null) {
+            res.json(vitamin);
+        } else {
+            res.status(404).json({ Error: 'The resource you are trying to locate does not exist.'  });
+        }         
+     })
+    .catch(error => {
+        console.log(error);
+        res.status(400).json({ Error: 'The document was not able to be compiled, check parameters again.' });
+    });
+
+});
+
+app.put('/VitaminPage/:_id', (req, res) => {
+    vitamins.updateVitamin(
+        req.params._id, 
+        req.body.name, 
+        req.body.date, 
+        req.body.purpose,
+        req.body.unit_day
+    )
+    .then(vitamin => {
+        res.json(vitamin);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(400).json({ error: 'Document was not able to be compiled, check parameters again.' });
+    });
+});
+
+app.delete('/VitaminPage/:_id', (req, res) => {
+    vitamins.deleteVitaminById(req.params._id)
+        .then(deletedCount => {
+            if (deletedCount === 1) {
+                res.status(204).send();
+            } else {
+                res.status(404).json({ Error:'The resource you are trying to locate does not exist.' });
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(400).json({ Error: 'Document was not able to be compiled, check parameters again.' });
+        });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
---
+++ backend-welchmea/model.mjs
@@ -0,0 +1,68 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+mongoose.connect(
+    process.env.MONGODB_CONNECT_STRING,
+    { useNewUrlParser: true }
+);
+const vitamin_db = mongoose.connection;
+
+// Confirm that the database has connected and print a message in the console.
+vitamin_db.once("open", (err) => {
+    if(err){
+        res.status(500).json({ error: 'Internal Server Error. The server was unable to process this request.' });
+    } else  {
+        console.log('You have successfully connected to MongoDB Vitamins collection using Mongoose.');
+    }
+});
+
+const vitaminSchema = mongoose.Schema({
+	name:      { type: String, required: true },
+	date:     { type: Date, required: true, default:Date.now },
+	purpose:       { type: String, required: true },
+    unit_day:        { type: Number, required: true },
+});
+
+const Vitamin = mongoose.model('Vitamins', vitaminSchema);
+
+const createVitamin = async (name, date, purpose, unit_day) => {
+    const vitamins = new Vitamin({ 
+        name:name,
+        date:date,
+        purpose:purpose,
+        unit_day:unit_day,
+    });
+    return vitamins.save();
+}
+
+const retrieveVitamins = async () => {
+    const query = Vitamin.find();
+    return query.exec();
+}
+
+const retrieveVitaminByID = async (_id) => {
+    const query = Vitamin.findById({_id: _id});
+    return query.exec();
+}
+
+const deleteVitaminById = async (_id) => {
+    const result = await Vitamin.deleteOne({_id: _id});
+    return  result.deletedCount;
+};
+
+const updateVitamin = async (_id, name, date, purpose, unit_day) => {
+    const result = await Vitamin.replaceOne({_id: _id }, {
+        name:name,
+        date:date,
+        purpose:purpose,
+        unit_day:unit_day,
+    });
+    return { 
+        name:name,
+        date:date,
+        purpose:purpose,
+        unit_day:unit_day, 
+    }
+}
+
+export { createVitamin, retrieveVitamins, retrieveVitaminByID, updateVitamin, deleteVitaminById }
