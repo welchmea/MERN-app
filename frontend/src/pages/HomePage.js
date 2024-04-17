4f2a76c101e26b1dc77c82c0c84877832402d44b
Meagan <welchmea@oregonstate.edu>
2024-04-17 13:26:02 -0500

    modified data on home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -8,16 +8,28 @@ function HomePage() {
   return (
     <>
       <div className="img-div-main">
-        <div className="icon-hover-main-transform" style={{ marginRight: 200, width: 'full'}}>
+        <div
+          className="icon-hover-main-transform"
+          style={{ marginRight: 200, width: "full" }}
+        >
           <DiMongodb />
         </div>
-        <div className="icon-hover-main-transform" style={{ marginLeft: 200, width: 'full'}}>
+        <div
+          className="icon-hover-main-transform"
+          style={{ marginLeft: 200, width: "full" }}
+        >
           <SiExpress />
         </div>
-        <div className="icon-hover-main-transform" style={{ marginRight: 200, width: 'full'}}>
+        <div
+          className="icon-hover-main-transform"
+          style={{ marginRight: 200, width: "full" }}
+        >
           <SiReact />
         </div>
-        <div className="icon-hover-main-transform" style={{ marginLeft: 200, width: 'full'}}>
+        <div
+          className="icon-hover-main-transform"
+          style={{ marginLeft: 200, width: "full" }}
+        >
           <SiNodedotjs />
         </div>
       </div>
@@ -30,68 +42,90 @@ function HomePage() {
           </p>
           <p>A MERN stack is made up of MongoDB, Express, React, and Node. </p>
           <ul>
-            <li>
-              Node is an open source library that allows for running web
-              applications outside the browser.
-            </li>
-            <li>
-              MongoDB is a document-oriented database, it provides support for
-              storage of JSON documents and allows for efficient queries.{" "}
-            </li>
-            <li>
-              Mongoose is specifically for use with Node.js and provides an
-              Object Data Modeling library, for MongoDB.
-            </li>
-            <li>
-              Express provides a framework for building API's while working in
-              Node.js. It assists in managing communication between servers and
-              routes.
-            </li>
-            <li>
-              React.js is used for the front end, to render the user interface.
-              It is a single page application, that downloads the necessary
-              files from the server once.
-            </li>
+            <ul>
+              Frontend: Emphasis is on the user experience
+              <li>
+                React is a JavaScript library that is used to build single page
+                applications (SPA) to render the user interface (UI).
+              </li>
+            </ul>
+
+            <ul>
+              Backend: Focuses on producing the functionality of the application
+              <li>
+                Node.js is a server side, open source, cross-platform runtime
+                environment for JavaScript. It allows a developer to utilize
+                JavaScript outside of a browser, meaning they can build their
+                backend without having to learn a new programming language.
+              </li>
+              <li>
+                MongoDB is a NoSQL, document oriented database. It stores data
+                in Collections vs. the traditional Tables that one might see in
+                a Relational Database Management System (RDBMS). A document in
+                this context is defined by key-value pairs (JSON-like). There
+                are no schemas defining the data. This unstructured format
+                provides flexibility as data within each Collection can have
+                varying fields.{" "}
+                <li>
+                  Mongoose is built on top of Node.js's MongoDB driver. If the
+                  developer prefers to maintain a schema-like approach to data
+                  storage, Mongoose is a provision for that capability. In this
+                  context it is referred to as a Object Data Model (ODM).
+                </li>
+              </li>
+              <li>
+                Express is another framework that is built on top of Node.js. It
+                supports request handling with Middleware and HTTP methods when
+                building APIs.
+              </li>
+            </ul>
           </ul>
 
           <p>
-            {" "}
-            The basic building blocks for implementing each component of this
-            MERN stack are JavaScript, HTML, CSS, Goggle Fonts, and React icons.
-          </p>
-          <ul>
-            <li>
-              JavaScript is an Object-Oriented Programming language that can be
-              used to develop both the frontend and backend. It is the language
-              used to write React applications.{" "}
-            </li>
-            <li>
-              HTML is the standard language for creating websites, it
-              essentially tells the browser how to display the static content.
-            </li>
-            <li>
-              CSS has been utilized as an external stylesheet to style the
-              pages.
-            </li>
-            <li>
-              One of the choices for styling is done by importing Google Fonts,
-              which is a library of font families.
-            </li>
-            <li>
-              The icons were imported from the react-icon library and rendered
-              as components.
-            </li>
-            <li>
-              Images have been optimized to proper specifications in order to
-              support efficient load times in the browser.
-            </li>
-          </ul>
-
-          <p>
-            Finally, the backend also utilized REST API. REST is a structural
-            style for building an application program interface (API), in this
-            case through Express, that uses HTTP requests to perform CRUD
-            operations.
+            It is also important to mention that this application demonstrates a
+            RESTful style architecture.
+            <ul>
+              REST Stands for: Representational State Transfer.
+              <li>
+                This is a common approach to development that is essentially a
+                set of guidelines that define how two computers communicate.
+                <ul>i.e How Client and Server 'talk'</ul>
+              </li>
+              <li>
+                The client sends a request and the server sends a response.
+                <ul>
+                  Request: HTTP Methods that are equivalent to CRUD
+                  <li>A client sends the operation (PUT, POST, GET, DELETE) to endpoint on the server</li>
+                  <li>This can include Headers and Parameters</li>
+                  <li>
+                    If there is a 500 status code error, the Client can retry
+                    the request (GET, PUT, and DELETE are idempotent)
+                  </li>
+                  Response: Typically returned as JSON data
+                  <li>First line will return a proper HTTP status code</li>
+                  <li>
+                    HTTP Status Codes: 200 - success, 400 - client side error, 500 -
+                    server side error
+                  </li>
+                  <li>
+                    If there is a huge amount of data being returned from the
+                    server use pagination (limit and offset parameters)
+                  </li>
+                </ul>
+              </li>
+              <li>
+                The benefits of a REST API are:
+                <ul>
+                  <li>Simple and Standardized</li>
+                  <li>
+                    Scalable and Stateless Stateless: Server DOES NOT store
+                    client's data and vice-versa, Request/Response are
+                    independent.
+                  </li>
+                  <li>High Performance due to caching support.</li>
+                </ul>
+              </li>
+            </ul>
           </p>
         </article>
       </div>
